95741d9732e694176512c20e3197627d8940c8b5
Anasxon <96531347+Anasxon5546@users.noreply.github.com>
2023-12-24 15:36:31 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+const jokeContainer = document.getElementById('joke');
+const btn = document.getElementById('btn');
+const url = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,religious,political,racist,sexist,explicit";
+
+let getJoke = () => {
+  jokeContainer.classList.remove('fade');
+  fetch(url)
+    .then(response => response.json())
+    .then(item => {
+      if (item.type === 'twopart') {
+        jokeContainer.textContent = `${item.setup} ${item.delivery}`;
+      } else {
+        jokeContainer.textContent = `${item.joke}`;
+      }
+      jokeContainer.classList.add('fade');
+    });
+}
+
+btn.addEventListener('click', getJoke);
+getJoke();
